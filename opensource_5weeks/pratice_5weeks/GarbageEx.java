a3e35cdbcb50151a3c3f7c51b59e3815aed72ab3
Minseonhong <tjsghd456@naver.com>
2021-10-01 14:49:07 +0900

    pratice_5weeks

---
+++ opensource_5weeks/pratice_5weeks/GarbageEx.java
@@ -0,0 +1,11 @@
+package pratice_5weeks;
+
+public class GarbageEx {
+
+	public static void main(String[] args) {
+		String a = new String("Good");
+		String b = new String("Bad");
+		String c = new String("Normal");
+		String d, e; a = null; d = c; c = null;
+	}
+}
